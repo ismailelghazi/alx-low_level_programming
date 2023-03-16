3ad96288f5b3b1e97f71fc19c0f555ca912b7bfa
ismailelghazi <ismail47el@gmail.com>
2023-03-16 16:40:23 -0700

    C-1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,15 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description:
+ * 	ssize_t write(int fd, const void *buf , size_t count);
+ * Return: 1 (error).
  */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
+
+int main(void)
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
