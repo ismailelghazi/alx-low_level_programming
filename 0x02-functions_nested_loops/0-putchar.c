c7d204314bdb789a2035bb97a5c1361f2b3abe37
ismailelghazi <ismail47el@gmail.com>
2023-03-22 18:20:57 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "main.h"
 /**
- * main - Entry point
- *
- * Description: print _putchar.
- *
- * Return: Always 0 (Success)
-*/
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
+
